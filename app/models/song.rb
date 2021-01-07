42a6585f08c5640e4953637336140f547ac98f18
Simran <mdrsimran@gmail.com>
2021-01-07 07:15:45 -0600

    commit tree 4ffdd46c303f5bf0e4f21eafa644429a7100b8a4

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,38 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  # accepts_nested_attributes_for :artist
+  # accepts_nested_attributes_for :genre
+  # accepts_nested_attributes_for :notes
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if content.strip != ""
+         self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+  end
+
 end
